294212add723010f06b9fe2ab8c24ec7f850eec6
TulgaAnkh <kekerhonor3@gmail.com>
2020-08-01 10:20:11 +0900

    first commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title + " - " + str(self.created)[:10]
+
+class Category(models.Model):
+    name = models.CharField(max_lenght=50)
+    path = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
